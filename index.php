1242d391acd817318c7eba5d2803ba16d8deef36
System Administrator <root@Erics-MacBook-Air.local>
2017-05-20 22:31:43 -0700

    parse php links to file

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
+<!--
+################################################################################
+# CODE DAY 2017
+# ERIC, DANIEL, ANDREW, ALEX, CODY, CHUAN
+################################################################################
+!-->
+
 <?php
 $link1 = $_POST['link1'];
 $link2 = $_POST['link2'];
+$link3 = $_POST['link3'];
 ?>
 
 <html>
 <body>
 <title>SortMe</title>
+
 <div align = "center"><font face = "Tahoma" size = "300">SortMe</font>
 <br><br><br>
 
@@ -17,24 +26,22 @@ $myfile = fopen("links.txt", "w") or die("Unable to open file!");
 fwrite($myfile, $link1);
 fwrite($myfile, "\n");
 fwrite($myfile, $link2);
+fwrite($myfile, "\n");
+fwrite($myfile, $link3);
 fclose($myfile);
 
 exec('python main.py');
+
 ?>
 
 Link 1: <input type = "text" name = "link1"></input><br>
 Link 2: <input type = "text" name = "link2"></input><br>
+Link 3: <input type = "text" name = "link3"></input><br>
 <br>
 
-<button> Go!</button>
+<button>Go!</button>
 <br>
 </div>
-
-
-
-<?php
-$newdosage = 1;
-?>
 
 </body>
 </html>
