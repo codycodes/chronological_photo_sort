e81df997f216206f704283d07141a28729d8a000
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-05-20 17:11:05 -0700

    Face API Requests Added

---
+++ face.py
@@ -0,0 +1,28 @@
+########### Python 2.7 #############
+import httplib, urllib, base64
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '30a8fd3d505148edb7302a354cde7370',
+}
+
+params = urllib.urlencode({
+    # Request parameters
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age',
+})
+
+# Replace the example URL below with the URL of the image you want to analyze.
+body = "{ 'url': 'http://i.imgur.com/vSczerS.jpg' }"
+
+try:
+    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
