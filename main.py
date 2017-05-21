4447f74d51e41efb0e452722accff81d0d15de40
Eric Sund <epsund@gmail.com>
2017-05-20 18:08:49 -0700

    age outputting works now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ########### Python 2.7 #############
-import httplib, urllib, base64, json
+import httplib, urllib, base64, json, time
 
 headers = {
     # Request headers
@@ -15,18 +15,25 @@ params = urllib.urlencode({
 })
 
 # Replace the example URL below with the URL of the image you want to analyze.
-body = "{'url' : 'http://i.imgur.com/kl5UuO1.png' }"
+photoUrls = ["http://i.imgur.com/kl5UuO1.png", "https://i.imgur.com/u1XBCp4.jpg"]
 
-try:
-    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+for url in photoUrls:
 
-data = json.loads(data)
-print json.dumps(data, indent=4, sort_keys=True)
-print data[0]['faceAttributes']['age']
+    body = "{'url' : \'" + url + "\'}"
+    print (body)
+
+    try:
+        conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+        conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+    data = json.loads(data)
+    print json.dumps(data, indent=4, sort_keys=True)
+    print data[0]['faceAttributes']['age']
+
+    time.sleep(3)
