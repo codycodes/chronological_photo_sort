66268a6575db9bedc482af2ad5fe7885f68eb987
System Administrator <root@Erics-MacBook-Air.local>
2017-05-20 20:39:19 -0700

    launch python

--- index.php
+++ index.php
@@ -9,23 +9,10 @@ $link2 = $_POST['link2'];
 <div align = "center"><font face = "Tahoma" size = "300">SortMe</font>
 <br><br><br>
 
-
 <form action = "index.php" method = "post">
-<script>
-  function onInitFs(fs) {
-
-  fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
-
-    fileEntry.isFile === true
-    fileEntry.name == 'log.txt'
-    fileEntry.fullPath == '~/log.txt'
-
-  }, errorHandler);
-
-}
-
-window.requestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, errorHandler);
-</script>
+<?php
+exec('python main.py');
+?>
 
 Link 1: <input type = "text" name = "link1"></input><br>
 Link 2: <input type = "text" name = "testvol"></input><br>
